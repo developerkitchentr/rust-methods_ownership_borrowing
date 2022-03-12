d517be1280fec98f2071cba842a517973bd0fe6c
Uygun Bodur <developerkitchentr@gmail.com>
2022-03-12 13:01:58 +0300

    Step 1: struct yapısı kuruldu

---
+++ README.md
@@ -0,0 +1,5 @@
+## RUST EXAMPLE METHODS
+### Rust ile yazdığımız bir kodu adım adı refactoring yapıyoruz.
+
+-1 Öncelikle yazdığımız Hands ve Item struct'ları ile basit bir yapı oluşturuyoruz. 
+Burada elde tutulan meyveleri simule edeceğiz. Lütfen kodun ilk halini dikkatlice izleyin.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+
+struct Item {
+    what: String,
+    present: bool,
+}
+
+struct Hands {
+    left: Item,
+    right: Item,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let hands: Hands = Hands {
+        left: Item {
+            what: "an apple".to_owned(),
+            present: true,
+        },
+        right: Item {
+            what: "an banana".to_owned(),
+            present: true,
+        },
+    };
+    
+    if hands.left.present {
+        println!("Left hand is holding {}", hands.left.what);
+    } else {
+        println!("Left hand is not holding!");
+    }
+
+    if hands.right.present {
+        println!("Right hand is holding {}", hands.right.what);
+    } else {
+        println!("Right hand is not holding!");
+    }
 }
