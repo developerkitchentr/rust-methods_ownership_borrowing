4ad386c589c1c65285a5ac088ac1c415df38437e
Uygun Bodur <developerkitchentr@gmail.com>
2022-03-12 13:18:35 +0300

    step2: swap örneği ve clippy

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 ## RUST EXAMPLE METHODS
 ### Rust ile yazdığımız bir kodu adım adı refactoring yapıyoruz.
 
--1 Öncelikle yazdığımız Hands ve Item struct'ları ile basit bir yapı oluşturuyoruz. 
+*Step 1:* Öncelikle yazdığımız Hands ve Item struct'ları ile basit bir yapı oluşturuyoruz. 
 Burada elde tutulan meyveleri simule edeceğiz. Lütfen kodun ilk halini dikkatlice izleyin.
+*Step 2:* Swap işlemi yapılıyor. Swap işlemi mutable değişkenler üzerinde yapılsa da rust 
+bu durumda bile bize biw uyarı verir.
+``
+ help: try: `std::mem::swap(&mut hands.left, &mut hands.right)
+``
+std::mem::swap bize önerdiği metotdur. Buna alternatif olarak clippy'e swap işlemini
+kabul etmesini belirtmek için şunu da main fonksiyonuna tanımlayabiliriz.
+``
+#[allow(clippy::manual_swap)]
+
+``
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ struct Hands {
     right: Item,
 }
 
+#[allow(clippy::manual_swap)]
 fn main() {
-    let hands: Hands = Hands {
+
+    // air ile yaptığımız swap işleminin işe yaraması için hands
+    // değişkenini mutable yapıyoruz.
+    let mut hands: Hands = Hands {
         left: Item {
             what: "an apple".to_owned(),
             present: true,
@@ -21,6 +25,28 @@ fn main() {
         },
     };
     
+    if hands.left.present {
+        println!("Left hand is holding {}", hands.left.what);
+    } else {
+        println!("Left hand is not holding!");
+    }
+
+    if hands.right.present {
+        println!("Right hand is holding {}", hands.right.what);
+    } else {
+        println!("Right hand is not holding!");
+    }
+
+    println!("Let's juggle");
+
+    // cargo clippy ile hatalarımızı kontrol ettiğimizde swap işleminde uyarı vermektedir
+    // this looks like you are swapping `hands.left` and `hands.right` manually
+    // Aşağıdaki metodu kullanmamızı öneriyor.
+    // std::mem::swap(&mut hands.left, &mut hands.right)
+    let air = hands.left;
+    hands.left = hands.right;
+    hands.right = air;  
+
     if hands.left.present {
         println!("Left hand is holding {}", hands.left.what);
     } else {
